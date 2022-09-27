411d75637f2d499fefc18be3e308cf1cd83c759e
Jin1won <wlsdnjs16521@gmail.com>
2022-09-28 02:21:18 +0900

    refactor: 약물 상세 페이지 URL 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -10,6 +11,8 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
   compiler: {
     styledComponents: true,
